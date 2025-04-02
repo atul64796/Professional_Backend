6bf51ade501496b2a2bf36685a2652afbf2169b2
Atul Kumar Chaurasia <atulkumarchourasia64796@gmail.com>
2025-04-02 16:13:35 +0530

    Fixed typos in ApiError class and improved error handling

--- src/index.js
+++ src/index.js
@@ -8,6 +8,31 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at port : ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("MONGO Db connect fail !!! ",err)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*
 import express from "express"
