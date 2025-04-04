a2a9e9b1803db773019f24b63edb699039df30b2
Atul Kumar Chaurasia <atulkumarchourasia64796@gmail.com>
2025-04-04 16:09:13 +0530

    Updated JWT login logic

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,7 +3,7 @@ class ApiError extends Error{
         statusCode,
         message= "Something went wrong",
         errors = [],
-        stack = ""
+        stack = "",
         
     ){
         super(message)
@@ -11,7 +11,7 @@ class ApiError extends Error{
         this.data = null
         this.message = message
         this.sucess = false
-        this.errors = this.errors
+        this.errors = errors
 
         if(stack){
             this.stack = stack
