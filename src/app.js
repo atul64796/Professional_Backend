2f24da8b8a636af93938ef15ee0f4987cdf714b9
Atul Kumar Chaurasia <atulkumarchourasia64796@gmail.com>
2025-04-14 12:29:15 +0530

    added subscription model in model foler

--- src/app.js
+++ src/app.js
@@ -3,13 +3,23 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 
 
+
+
+
+
 const app = express();
 
 
+// app.use(cors({
+//     origin: process.env.CORS_ORIGIN,
+//     credentials:true
+// }));
+
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: "http://localhost:5173" ,
     credentials:true
-}));
+}))
+
 
 app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({extended:true,limit:"16kb"}));
